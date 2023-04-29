cc1565a04468b41f009f83a896042d92d0fc4992
Jianzhe Gao <37926649+Gaozzzz@users.noreply.github.com>
2023-04-29 10:18:39 +0800

    Update Pred.py

--- lib/network/module/Pred.py
+++ lib/network/module/Pred.py
@@ -12,9 +12,9 @@ class Out_Pred(nn.Module):
     def forward(self, decoder):  # decoder = [decoder1, decoder2, decoder3, decoder4]
         decoder1, decoder2, decoder3, decoder4 = decoder
 
-        decoder1 = decoder1  # 16, 64, 88, 88
-        decoder2 = F.interpolate(decoder2, scale_factor=2, mode='bilinear', align_corners=False)  # 16, 128, 88, 88
-        decoder3 = F.interpolate(decoder3, scale_factor=4, mode='bilinear', align_corners=False)  # 16, 320, 88, 88
+        decoder1 = decoder1
+        decoder2 = F.interpolate(decoder2, scale_factor=2, mode='bilinear', align_corners=False)
+        decoder3 = F.interpolate(decoder3, scale_factor=4, mode='bilinear', align_corners=False)
 
         pred = self.conv_pred(torch.cat([decoder3, decoder2, decoder1], dim=1))
         return pred
