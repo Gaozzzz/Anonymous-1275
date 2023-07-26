22af71559f698e691e5883dd144bc83305ed2e2d
Jianzhe Gao <37926649+Gaozzzz@users.noreply.github.com>
2023-07-26 17:46:26 +0800

    Update Decoder.py

--- lib/network/module/Decoder.py
+++ lib/network/module/Decoder.py
@@ -5,7 +5,52 @@ from mmcv.cnn import ConvModule
 from .utils import Linear, Skip_Concat
 
 
-class Decoder(nn.Module):
+class Decoder_1(nn.Module):
+    def __init__(self, embed_dims=[64, 128, 320, 512]):
+        super(Decoder, self).__init__()
+        #   skip concat
+        self.skip_concat = Skip_Concat()
+
+        #   encoder4 + decoder4 = decoder3
+        self.conv_concat34 = ConvModule(in_channels=(embed_dims[4] + embed_dims[3]),
+                                        out_channels=embed_dims[2], kernel_size=1,
+                                        norm_cfg=dict(type='BN', requires_grad=True))
+        self.linear_concat34 = Linear(input_dim=embed_dims[2], embed_dim=embed_dims[2])
+
+        #   encoder3 + decoder3 = decoder2
+        self.conv_concat23 = ConvModule(in_channels=(embed_dims[1] + embed_dims[2]),
+                                        out_channels=embed_dims[1], kernel_size=1,
+                                        norm_cfg=dict(type='BN', requires_grad=True))
+        self.linear_concat23 = Linear(input_dim=embed_dims[1], embed_dim=embed_dims[1])
+
+        #   encoder2 + decoder2 = decoder1
+        self.conv_concat12 = ConvModule(in_channels=(embed_dims[0] + embed_dims[1]),
+                                        out_channels=embed_dims[0], kernel_size=1,
+                                        norm_cfg=dict(type='BN', requires_grad=True))
+        self.linear_concat12 = Linear(input_dim=embed_dims[0], embed_dim=embed_dims[0])
+
+    def forward(self, encoder):  # encoder = encoder1, encoder2, cl_encoder3, cl_encoder4
+        encoder1, encoder2, encoder3, encoder4 = encoder
+
+        #   decoder3
+        decoder3 = self.skip_concat(encoder3, decoder4)
+        decoder3 = self.linear_concat34(self.conv_concat34(decoder3))
+
+        #   decoder2
+        decoder3_up2 = F.interpolate(decoder3, scale_factor=2, mode='bilinear', align_corners=False)
+        decoder2 = self.skip_concat(encoder2, decoder3_up2)
+        decoder2 = self.linear_concat23(self.conv_concat23(decoder2))
+
+        #   decoder1
+        decoder2_up2 = F.interpolate(decoder2, scale_factor=2, mode='bilinear', align_corners=False)
+        decoder1 = self.skip_concat(encoder1, decoder2_up2)
+        decoder1 = self.linear_concat12(self.conv_concat12(decoder1))
+
+        decoder = [decoder1, decoder2, decoder3, decoder4]
+        return decoder
+
+
+class Decoder_2(nn.Module):
     def __init__(self, embed_dims=[64, 128, 320, 512]):
         super(Decoder, self).__init__()
         #   skip concat
